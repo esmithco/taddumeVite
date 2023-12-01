a56dfd5980838d5be993225db68ade8bff3f051d
Brayan Mendoza <esmith@192.168.1.2>
2023-12-01 11:40:22 -0500

    Updates

--- src/components/ButtonSecondary/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ButtonSecondary.css";
-
-function ButtonSecondary(props) {
-  return <button className="ButtonSecondary">{props.text}</button>;
-}
-
-export { ButtonSecondary };
