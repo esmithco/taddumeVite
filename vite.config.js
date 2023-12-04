2f27b633e5d86678da34d45e28bd7163105c3a79
Brayan Mendoza <esmith@192.168.1.2>
2023-12-04 08:54:11 -0500

    cambiando el link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://esmithco.github.io/taddumeVite'
+  base: 'http://taddume.com'
 })
